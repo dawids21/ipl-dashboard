c0a65c236a21786e8ba071e627a4febcd6e68830
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-18 22:15:53 +0200

    write Batch Config

---
+++ backend/src/main/java/xyz/stasiak/ipldashboard/data/JobCompletionNotificationListener.java
@@ -0,0 +1,34 @@
+package xyz.stasiak.ipldashboard.data;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+    private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+            log.info("!!! JOB FINISHED! Time to verify the results");
+
+            jdbcTemplate.query("SELECT team1, team2, date FROM match",
+                               (rs, row) -> "Team 1 " + rs.getString("team1") + " Team 2 " + rs.getString("team2") +
+                                            " Date " + rs.getString("date"))
+                        .forEach(System.out::println);
+        }
+    }
+}
